d93820003556ae1fb22aa03667fc8f1612416609
ivan <vansmuhammad@gmail.com>
2019-05-29 04:31:53 +0700

    add: input field with redux-form

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-function App() {
-  return <div>Stream Create</div>;
+function App(props) {
+  console.log(props);
+
+  const renderInput = ({ input, label }) => {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
+  };
+
+  return (
+    <form className="ui form">
+      <Field name="Title" component={renderInput} label="Enter Title" />
+      <Field
+        name="Description"
+        component={renderInput}
+        label="Enter Description"
+      />
+    </form>
+  );
 }
 
-export default App;
+export default reduxForm({
+  form: "Stream_Create"
+})(App);
