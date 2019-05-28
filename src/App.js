2c46581e98e9c35f9d1a1c3cf03fdb791e5931fa
ivan <vansmuhammad@gmail.com>
2019-05-28 21:01:54 +0700

    add: stream create to router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import List from "./List";
 import Header from "./Header";
 
+import Create from "./Create";
+
 function App() {
   return (
     <div>
@@ -10,7 +12,7 @@ function App() {
         <div>
           <Header />
           <Route exact path="/" component={List} />
-          <Route />
+          <Route path="/Stream/Create" component={Create} />
           <Route />
           <Route />
           <Route />
