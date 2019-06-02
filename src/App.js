f3af1dc959db5f1d47bdb42cfac01c4bde55d777
ivan <vansmuhammad@gmail.com>
2019-06-02 14:35:27 +0700

    add: new route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import List from "./List";
 import Header from "./Header";
+import history from "./History";
 
 import Create from "./Create";
+import Edit from "./Edit";
 
 function App() {
   return (
     <div>
-      <Router>
+      <Router history={history}>
         <div>
           <Header />
           <Route exact path="/" component={List} />
           <Route path="/Stream/Create" component={Create} />
-          <Route />
+          <Route path="/Stream/Edit/:id" component={Edit} />
           <Route />
           <Route />
         </div>
