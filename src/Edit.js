bdf551852b26e24e779653bfcd98110eb78caf67
ivan <vansmuhammad@gmail.com>
2019-06-02 14:37:17 +0700

    add: new edit stream file

---
+++ src/Edit.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchStreams } from "./actions";
+import { NavLink } from "react-router-dom";
+
+function App(props) {
+  return (
+    <div>
+      <h2>Streams</h2>
+    </div>
+  );
+}
+
+export default App;
