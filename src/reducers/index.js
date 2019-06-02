f7ad286ff23a0126024431c8ba84ef3ac23cf9a1
ivan <vansmuhammad@gmail.com>
2019-06-02 14:36:40 +0700

    add: reducer for stream

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,12 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 import Auth from "./Auth";
+import streamReducer from "./Stream";
 
-const reducer = combineReducers({ Auth, form: formReducer });
+const reducer = combineReducers({
+  auth: Auth,
+  form: formReducer,
+  streams: streamReducer
+});
 
 export default reducer;
