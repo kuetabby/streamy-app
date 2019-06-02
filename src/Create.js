a2b6c7f1379ad3fc1e65495e3df72aa8317fbb3a
ivan <vansmuhammad@gmail.com>
2019-06-02 14:39:30 +0700

    fix: new ui with more logic

--- src/Create.js
+++ src/Create.js
@@ -1,30 +1,69 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "./actions";
 
 function App(props) {
+  const { createStream } = props;
   console.log(props);
 
-  const renderInput = ({ input, label }) => {
+  const renderError = ({ error, touched }) => {
+    if (error && touched) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  };
+
+  const renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
     return (
-      <div className="field">
+      <div className={className}>
         <label>{label}</label>
-        <input {...input} />
+        <input {...input} autoComplete="off" />
+        {renderError(meta)}
       </div>
     );
   };
 
+  const submit = values => {
+    createStream(values);
+  };
+
   return (
-    <form className="ui form">
-      <Field name="Title" component={renderInput} label="Enter Title" />
+    <form className="ui form error" onSubmit={props.handleSubmit(submit)}>
+      <Field name="title" component={renderInput} label="Enter Title" />
       <Field
-        name="Description"
+        name="description"
         component={renderInput}
         label="Enter Description"
       />
+      <button className="ui button primary">Submit</button>
     </form>
   );
 }
 
-export default reduxForm({
-  form: "Stream_Create"
+const validate = values => {
+  const errors = {};
+  if (!values.title) {
+    errors.title = "Title must not empty";
+  }
+  if (!values.description) {
+    errors.description = "Description must not empty";
+  }
+  return errors;
+};
+
+const formWrapped = reduxForm({
+  form: "Stream_Create",
+  validate
 })(App);
+
+export default connect(
+  null,
+  {
+    createStream
+  }
+)(formWrapped);
