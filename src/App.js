ec544f1d5d8e933253de956e4ec5ceab90161678
ivan <vansmuhammad@gmail.com>
2019-06-06 12:20:14 +0700

    add: new route & switch to route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route } from "react-router-dom";
+import { Router, Switch, Route } from "react-router-dom";
 import history from "./History";
 
 import Header from "./components/reusable/Header";
@@ -7,19 +7,20 @@ import List from "./components/stream/List";
 import Create from "./components/stream/Create";
 import Edit from "./components/stream/Edit";
 import Deleted from "./components/stream/Delete";
+import Show from "components/stream/Show";
 
 function App() {
   return (
     <div>
       <Router history={history}>
-        <div>
-          <Header />
+        <Header />
+        <Switch>
           <Route exact path="/" component={List} />
           <Route path="/Stream/Create" component={Create} />
           <Route path="/Stream/Edit/:id" component={Edit} />
           <Route path="/Stream/Delete/:id" component={Deleted} />
-          <Route />
-        </div>
+          <Route path="/Stream/:id" component={Show} />
+        </Switch>
       </Router>
     </div>
   );
