3adc3fa827c373419176cdce890f2f42e74a876c
ivan <vansmuhammad@gmail.com>
2019-06-07 18:43:06 +0700

    add: video to stream

--- src/components/stream/Show.js
+++ src/components/stream/Show.js
@@ -1,14 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { connect } from "react-redux";
+import flv from "flv.js";
 import { fetchStream } from "actions";
 
 function App(props) {
   const { fetchStream, show } = props;
   const id = props.match.params.id;
+  const videoRef = useRef();
+  const buildPlayer = useRef();
 
   useEffect(() => {
     fetchStream(id);
   }, [fetchStream, id]);
+
+  useEffect(() => {
+    if (show) {
+      setTimeout(() => {
+        buildPlayer.current = flv.createPlayer({
+          type: "flv",
+          url: `http://localhost:8000/live/${id}.flv`
+        });
+        buildPlayer.current.attachMediaElement(videoRef.current);
+        buildPlayer.current.load();
+      }, 100);
+    }
+    return clearTimeout(buildPlayer.current);
+  }, [id, show]);
 
   if (!show) {
     return <div>Loading ....</div>;
@@ -16,6 +33,7 @@ function App(props) {
 
   return (
     <div>
+      <video ref={videoRef} style={{ width: "100%" }} controls />
       <h1>{show.title}</h1>
       <h3>{show.description}</h3>
     </div>
